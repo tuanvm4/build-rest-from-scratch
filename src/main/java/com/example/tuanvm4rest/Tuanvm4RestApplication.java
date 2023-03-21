cb00ee07dce3f08e4425c740f94391e6ce378b77
vm tuan <tuanvm2510@gmail.com>
2023-03-21 23:17:25 +0700

    init

---
+++ src/main/java/com/example/tuanvm4rest/Tuanvm4RestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tuanvm4rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuanvm4RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tuanvm4RestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tuanvm4rest/Tuanvm4RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tuanvm4rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tuanvm4RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
